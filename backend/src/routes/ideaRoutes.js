3215d3435a5e32236013ea1e9fcee3c3ebe54a39
kshiprant <kshiprant19@gmail.com>
2026-03-11 21:24:15 +0530

    Update ideaRoutes.js

--- backend/src/routes/ideaRoutes.js
+++ backend/src/routes/ideaRoutes.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
-import { addComment, createIdea, getIdeas, toggleLike } from '../controllers/ideaController.js';
+import {
+  addComment,
+  createIdea,
+  getIdeas,
+  toggleLike,
+} from '../controllers/ideaController.js';
 
 const router = express.Router();
+
 router.use(protect);
-router.route('/').get(getIdeas).post(createIdea);
+
+/* Get all ideas / Create new idea */
+router.route('/')
+  .get(getIdeas)
+  .post(createIdea);
+
+/* Like or unlike idea */
 router.post('/:id/like', toggleLike);
+
+/* Add comment */
 router.post('/:id/comment', addComment);
+
 export default router;

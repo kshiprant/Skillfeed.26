1fbd5737c1da12037bdb6b421dd08f3919660d47
kshiprant <kshiprant19@gmail.com>
2026-03-13 03:30:19 +0530

    Update connectionRoutes.js

--- backend/src/routes/connectionRoutes.js
+++ backend/src/routes/connectionRoutes.js
@@ -1,10 +1,35 @@
 import express from 'express';
+import { body, param } from 'express-validator';
 import protect from '../middleware/authMiddleware.js';
-import { actOnRequest, getPendingRequests, sendConnectionRequest } from '../controllers/connectionController.js';
+import {
+  actOnRequest,
+  getPendingRequests,
+  sendConnectionRequest,
+} from '../controllers/connectionController.js';
 
 const router = express.Router();
+
 router.use(protect);
-router.post('/', sendConnectionRequest);
+
+router.post(
+  '/',
+  [
+    body('toUser').isMongoId().withMessage('Valid target user ID is required'),
+  ],
+  sendConnectionRequest
+);
+
 router.get('/', getPendingRequests);
-router.patch('/:id', actOnRequest);
+
+router.patch(
+  '/:id',
+  [
+    param('id').isMongoId().withMessage('Valid request ID is required'),
+    body('action')
+      .isIn(['accept', 'reject'])
+      .withMessage('Action must be accept or reject'),
+  ],
+  actOnRequest
+);
+
 export default router;
